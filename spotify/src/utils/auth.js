f5f8d3cc3740892624d3c87d0d1881969b8f5fe7
EZHO7518 <87016915+EZHO7518@users.noreply.github.com>
2024-06-10 01:38:37 +0900

    Update auth.js

--- spotify/src/utils/auth.js
+++ spotify/src/utils/auth.js
@@ -1,13 +1,14 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = 'afc9840c971641f3bd65ec60a2312b67';
 const redirectUri = process.env.NODE_ENV === 'production' 
-  ? 'http://ezho7518.github.io/spotify/spotify/callback' 
+  ? 'http://EZHO7518.github.io/spotify/callback' 
   : 'http://localhost:3000/callback';
 const scopes = [
   'user-top-read',
   'user-read-recently-played',
-  'user-modify-playback-state',
+  'playlist-read-private',
   'user-read-playback-state',
+  'user-modify-playback-state',
   'user-read-currently-playing',
   'user-read-playback-position',
   'user-library-read',
@@ -18,15 +19,15 @@ const scopes = [
 
 export const getTokenFromUrl = () => {
   const hash = window.location.hash.substring(1);
-  console.log('Hash:', hash);
+  console.log('Hash:', hash); // 디버깅 로그 추가
   const params = hash.split('&').reduce((acc, part) => {
     const [key, value] = part.split('=');
     acc[key] = decodeURIComponent(value);
     return acc;
   }, {});
-  console.log('Params:', params);
+  console.log('Params:', params); // 디버깅 로그 추가
   const token = params.access_token;
-  console.log('Extracted token:', token);
+  console.log('Extracted token:', token); // 디버깅 로그 추가
   return { access_token: token };
 };
 
