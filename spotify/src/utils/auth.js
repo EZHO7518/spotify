2bded77962d7c89eb316a2019b970d588b74a19c
EZHO7518 <87016915+EZHO7518@users.noreply.github.com>
2024-06-15 02:05:20 +0900

    1

--- spotify/src/utils/auth.js
+++ spotify/src/utils/auth.js
@@ -1,8 +1,6 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = 'afc9840c971641f3bd65ec60a2312b67';
-const redirectUri = process.env.NODE_ENV === 'production' 
-  ? 'https://ezho7518.github.io/spotify/callback' 
-  : 'http://localhost:3000/callback';
+const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
 const scopes = [
   'user-top-read',
   'user-read-recently-played',
@@ -19,6 +17,7 @@ const scopes = [
 
 export const getTokenFromUrl = () => {
   const hash = window.location.hash.substring(1);
+  console.log('Hash:', hash); // 디버깅 로그 추가
   const params = hash.split('&').reduce((acc, part) => {
     const [key, value] = part.split('=');
     if (key && value) {
@@ -26,7 +25,9 @@ export const getTokenFromUrl = () => {
     }
     return acc;
   }, {});
+  console.log('Params:', params); // 디버깅 로그 추가
   const token = params.access_token;
+  console.log('Extracted token:', token); // 디버깅 로그 추가
   return { access_token: token };
 };
 
